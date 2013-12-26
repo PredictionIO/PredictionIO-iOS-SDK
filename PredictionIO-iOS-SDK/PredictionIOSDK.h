947214a240b1d5a8382c9f0e71efe0f2652eac59
yf225 <yf225@cornell.edu>
2013-12-26 12:17:42 -0800

    created xcode project

---
+++ PredictionIO-iOS-SDK/PredictionIOSDK.h
@@ -0,0 +1,13 @@
+//
+//  PredictionIO_iOS_SDK.h
+//  PredictionIO-iOS-SDK
+//
+//  Created by Yanbin Feng on 12/26/13.
+//  Copyright (c) 2013 PredictionIO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PredictionIOSDK : NSObject
+
+@end
---
+++ PredictionIO-iOS-SDK/PredictionIOSDK.m
@@ -0,0 +1,13 @@
+//
+//  PredictionIO_iOS_SDK.m
+//  PredictionIO-iOS-SDK
+//
+//  Created by Yanbin Feng on 12/26/13.
+//  Copyright (c) 2013 PredictionIO. All rights reserved.
+//
+
+#import "PredictionIOSDK.h"
+
+@implementation PredictionIOSDK
+
+@end
